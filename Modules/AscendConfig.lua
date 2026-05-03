40874592bc2f49c16f981fe058a9d5a6ccd3944f
bao-2204 <tt6jmjk95q@privaterelay.appleid.com>
2026-05-03 21:11:12 +0700

    Create AscendConfig.lua

---
+++ Modules/AscendConfig.lua
@@ -0,0 +1,557 @@
+return {
+  ['GetRankRewards'] = 'function: 0xde6aff79e1a7324a',
+  ['RankRewards'] = {
+    [1] = {
+      ['MoneyPercent'] = 10,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [2] = {
+      ['MoneyPercent'] = 10,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [3] = {
+      ['MoneyPercent'] = 10,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [4] = {
+      ['MoneyPercent'] = 10,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [5] = {
+      ['MoneyPercent'] = 10,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [6] = {
+      ['MoneyPercent'] = 10,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [7] = {
+      ['MoneyPercent'] = 10,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [8] = {
+      ['MoneyPercent'] = 10,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [9] = {
+      ['MoneyPercent'] = 10,
+      ['LuckPercent'] = 5,
+      ['GemsPercent'] = 10
+    },
+    [10] = {
+      ['MoneyPercent'] = 15,
+      ['CooldownReduction'] = 2,
+      ['LuckPercent'] = 10,
+      ['GemsPercent'] = 15
+    }
+  },
+  ['GetRankIcon'] = 'function: 0x0d11f5040b453f5a',
+  ['MaxAscend'] = 10,
+  ['ButtonCooldown'] = 0.25,
+  ['GetRankRequirements'] = 'function: 0x21940131bc06bcfa',
+  ['RankIcons'] = {
+    [1] = 'rbxassetid://73223831278247',
+    [2] = 'rbxassetid://109792281239395',
+    [3] = 'rbxassetid://100564347384172',
+    [4] = 'rbxassetid://94538412772797',
+    [5] = 'rbxassetid://139880695567167',
+    [6] = 'rbxassetid://126958736045760',
+    [7] = 'rbxassetid://105349834722968',
+    [8] = 'rbxassetid://94538412772797',
+    [9] = 'rbxassetid://100564347384172',
+    [10] = 'rbxassetid://94538412772797',
+    [0] = 'rbxassetid://73061349447129'
+  },
+  ['RankRequirements'] = {
+    [1] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 2500</font>',
+        ['type'] = 'Level',
+        ['amount'] = 2500
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">125</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 125
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">10</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 10
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">10M</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 10000000
+      }
+    },
+    [2] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 5000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 5000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">400</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 400
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">25</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 25
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">50M</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 50000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">150</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 150
+      },
+      [6] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">5x</font> Void Fragment',
+        ['amount'] = 5,
+        ['itemName'] = 'Void Fragment'
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Limitless Ring',
+        ['amount'] = 2,
+        ['itemName'] = 'Limitless Ring'
+      }
+    },
+    [3] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 6000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 6000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">500</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 500
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">50</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 50
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">250M</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 250000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">300</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 300
+      },
+      [6] = {
+        ['display'] = 'Clear Shadow Dungeon (Easy) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearCIDEasy',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">7</font> Dismantle Fang',
+        ['amount'] = 7,
+        ['itemName'] = 'Dismantle Fang'
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">5x</font> Dark Ring',
+        ['amount'] = 5,
+        ['itemName'] = 'Dark Ring'
+      }
+    },
+    [4] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 7000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 7000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">625</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 625
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">50</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 50
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">1B</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 1000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">500</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 500
+      },
+      [6] = {
+        ['display'] = 'Clear Shadow Dungeon (Medium) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearCIDMedium',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">4x</font> Reiatsu Core',
+        ['amount'] = 4,
+        ['itemName'] = 'Reiatsu Core'
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">1x</font> Blood Ring',
+        ['amount'] = 1,
+        ['itemName'] = 'Blood Ring'
+      }
+    },
+    [5] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 8000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 8000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">750</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 750
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">150</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 150
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">5B</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 5000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">750</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 750
+      },
+      [6] = {
+        ['display'] = 'Clear Shadow Dungeon (Hard) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearCIDHard',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> HÅgyoku Fragment',
+        ['amount'] = 2,
+        ['itemName'] = 'HÅgyoku Fragment'
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Atomic Core',
+        ['amount'] = 2,
+        ['itemName'] = 'Atomic Core'
+      }
+    },
+    [6] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 8500</font>',
+        ['type'] = 'Level',
+        ['amount'] = 8500
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">850</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 850
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">250</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 250
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">20B</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 20000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">850</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 850
+      },
+      [6] = {
+        ['display'] = 'Clear Rune Dungeon (Medium) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearRuneMedium',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['display'] = 'Clear Shadow Dungeon (Medium) <font face="MontserratBold">5</font> Times',
+        ['type'] = 'ClearCIDMedium',
+        ['amount'] = 5
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">3x</font> Blood Ring',
+        ['amount'] = 3,
+        ['itemName'] = 'Blood Ring'
+      },
+      [9] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">3x</font> Soul Flame',
+        ['amount'] = 3,
+        ['itemName'] = 'Soul Flame'
+      }
+    },
+    [7] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 9000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 9000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">1000</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 1000
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">350</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 350
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">75B</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 75000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">1000</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 1000
+      },
+      [6] = {
+        ['display'] = 'Clear Rune Dungeon (Hard) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearRuneHard',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['display'] = 'Clear Shadow Dungeon (Hard) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearCIDHard',
+        ['amount'] = 10
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Cursed Flesh',
+        ['amount'] = 2,
+        ['itemName'] = 'Cursed Flesh'
+      },
+      [9] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Infinity Essence',
+        ['amount'] = 2,
+        ['itemName'] = 'Infinity Essence'
+      }
+    },
+    [8] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 10000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 10000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">1250</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 1250
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">500</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 500
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">350B</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 350000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">1250</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 1250
+      },
+      [6] = {
+        ['display'] = 'Clear Rune Dungeon (Hard) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearRuneHard',
+        ['amount'] = 10
+      },
+      [7] = {
+        ['display'] = 'Clear Shadow Dungeon (Hard) <font face="MontserratBold">10</font> Times',
+        ['type'] = 'ClearCIDHard',
+        ['amount'] = 10
+      },
+      [8] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Phantasm Core',
+        ['amount'] = 2,
+        ['itemName'] = 'Phantasm Core'
+      },
+      [9] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">3x</font> Slime Core',
+        ['amount'] = 3,
+        ['itemName'] = 'Slime Core'
+      }
+    },
+    [9] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 11500</font>',
+        ['type'] = 'Level',
+        ['amount'] = 11500
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">1500</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 1500
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">700</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 700
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">1T</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 1000000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">1500</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 1500
+      },
+      [6] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Azure Heart',
+        ['amount'] = 2,
+        ['itemName'] = 'Azure Heart'
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Evolution Fragment',
+        ['amount'] = 2,
+        ['itemName'] = 'Evolution Fragment'
+      },
+      [8] = {
+        ['display'] = 'Clear <font face="MontserratBold">15</font> Medium+ Boss Rushes',
+        ['type'] = 'ClearBossRushMediumPlus',
+        ['amount'] = 15
+      }
+    },
+    [10] = {
+      [1] = {
+        ['display'] = 'Reach <font face="MontserratBold">Level 13000</font>',
+        ['type'] = 'Level',
+        ['amount'] = 13000
+      },
+      [2] = {
+        ['display'] = 'Kill <font face="MontserratBold">2000</font> NPCs',
+        ['type'] = 'KillNPCs',
+        ['amount'] = 2000
+      },
+      [3] = {
+        ['display'] = 'Kill <font face="MontserratBold">00</font> Bosses',
+        ['type'] = 'KillBosses',
+        ['amount'] = 1000
+      },
+      [4] = {
+        ['display'] = 'Deal <font face="MontserratBold">7.5T</font> Damage',
+        ['type'] = 'DealDamage',
+        ['amount'] = 7500000000000
+      },
+      [5] = {
+        ['display'] = 'Use Abilities <font face="MontserratBold">2000</font> Times',
+        ['type'] = 'UseAbilities',
+        ['amount'] = 2000
+      },
+      [6] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">2x</font> Path Fragment',
+        ['amount'] = 2,
+        ['itemName'] = 'Path Fragment'
+      },
+      [7] = {
+        ['type'] = 'Item',
+        ['category'] = 'Items',
+        ['display'] = '<font face="MontserratBold">3x</font> Corrupt Crown',
+        ['amount'] = 3,
+        ['itemName'] = 'Corrupt Crown'
+      },
+      [8] = {
+        ['display'] = 'Reach Floor 75+ <font face="MontserratBold">2</font> Times in Infinite Tower',
+        ['type'] = 'ReachTowerFloor75',
+        ['amount'] = 2
+      }
+    }
+  },
+  ['GetItemRequirements'] = 'function: 0x1da1431ebe5a3d0a',
+  ['ValidBosses'] = {
+    [1] = 'SukunaBoss',
+    [2] = 'JinwooBoss',
+    [3] = 'GojoBoss',
+    [4] = 'SaberBoss',
+    [5] = 'YujiBoss',
+    [6] = 'AlucardBoss',
+    [7] = 'ShadowBoss',
+    [8] = 'QinShiBoss',
+    [9] = 'AizenBoss',
+    [10] = 'IchigoBoss',
+    [11] = 'RimuruBoss',
+    [12] = 'MadokaBoss'
+  },
+  ['GetRankName'] = 'function: 0x31dd19ed67a4beaa',
+  ['GetTotalRewards'] = 'function: 0x4c3a2ba186c0319a',
+  ['RankNames'] = {
+    [1] = 'Ascend I',
+    [2] = 'Ascend II',
+    [3] = 'Ascend III',
+    [4] = 'Ascend IV',
+    [5] = 'Ascend V',
+    [6] = 'Ascend VI',
+    [7] = 'Ascend VII',
+    [8] = 'Ascend VIII',
+    [9] = 'Ascend IX',
+    [10] = 'Ascend X',
+    [0] = 'None'
+  },
+  ['LiveRefreshRate'] = 1.5
+}
