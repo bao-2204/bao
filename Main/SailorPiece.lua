c8d6d29c01ca3e21702937fb473dcc561d91ab32
bao-2204 <tt6jmjk95q@privaterelay.appleid.com>
2026-05-09 10:06:07 +0700

    Update SailorPiece.lua

--- Main/SailorPiece.lua
+++ Main/SailorPiece.lua
@@ -1,1 +1,13 @@
-return loadstring(game:HttpGet("http://212.132.120.102:14976/get_script"))()
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local a, b = pcall(function() 
+    return game:HttpGet("http://212.132.120.102:14976/get_script")
+end)
+
+if a then
+  loadstring(b)()
+elseif not a then
+  LocalPlayer:Kick("Something went wrong please join our discord server \nhttps://discord.gg/9AfQVkY5wu")
+  setclipboard("https://discord.gg/9AfQVkY5wu")
+end
